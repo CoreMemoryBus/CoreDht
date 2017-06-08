c44456b72716d9e171abc749c7b99956bbadeda3
machonky <dev@macrobert.com>
2017-06-08 23:07:14 +0800

    Refactor Example

---
+++ sample/FirstExample/MyAppNode.cs
@@ -0,0 +1,11 @@
+using System;
+using CoreDht.Node;
+
+namespace FirstExample
+{
+    public class MyAppNode : Node
+    {
+        public MyAppNode(string binding, Action<string> logger) : base(binding, logger)
+        { }
+    }
+}
