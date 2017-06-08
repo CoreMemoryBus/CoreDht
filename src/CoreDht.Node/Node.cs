ad878e03c07dd517f569772550651cff5ff96bf7
machonky <dev@macrobert.com>
2017-06-08 22:58:12 +0800

    Refactoring Node to where it really belongs

---
+++ src/CoreDht.Node/Node.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace FirstExample
+{
+    public class Node
+    {
+        protected Action<string> Logger { get; }
+        protected DealerSocket ListeningSocket { get; }
+        protected NetMQPoller Poller { get; set; }
+        private PairSocket Shim { get; set; }
+
+        protected Node(string binding, Action<string> logger)
+        {
+            Logger = logger;
+            ListeningSocket = new DealerSocket(binding);
+            Poller = new NetMQPoller();
+            Actor = CreateActor();
+        }
+
+        public NetMQActor Actor { get; }
+
+        private NetMQActor CreateActor()
+        {
+            return NetMQActor.Create(shim =>
+            {
+                Shim = shim;
+                Shim.ReceiveReady += ShimOnReceiveReady;
+                Shim.SignalOK();
+
+                Poller.Add(Shim);
+                Poller.Add(ListeningSocket);
+                Poller.Run();
+
+                Logger?.Invoke("Node closed");
+            });
+        }
+
+
+        private void ShimOnReceiveReady(object sender, NetMQSocketEventArgs args)
+        {
+            var mqMsg = args.Socket.ReceiveMultipartMessage();
+            var typeCode = mqMsg[0].ConvertToString();
+            switch (typeCode)
+            {
+                case NetMQActor.EndShimMessage:
+                    Poller.Stop();
+                    Logger?.Invoke($"Node terminating.");
+                    break;
+
+            }
+        }
+    }
+}
