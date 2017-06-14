180c9d658d613e4a7432922f242cab456f209a68
machonky <dev@macrobert.com>
2017-06-14 21:30:34 +0800

    Socket Cache for outbound transmissions

---
+++ src/CoreDht.Node/OutgoingSocket.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using CoreDht.Utils;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace CoreDht.Node
+{
+    /// <summary>
+    /// This wrapper records if there were errors when attempting to send, 
+    /// so we don't need to test the socket by sending again.
+    /// </summary>
+    public class OutgoingSocket : IDisposable, IOutgoingSocket
+    {
+        private readonly IUtcClock _clock;
+        public IOutgoingSocket Socket { get; }
+        public bool Error { get; private set; }
+
+        public DateTime LastTransmission { get; private set; }
+
+        public OutgoingSocket(DealerSocket socket, IUtcClock clock)
+        {
+            _clock = clock;
+            Socket = socket;
+        }
+
+        public OutgoingSocket(NetMQActor socket, IUtcClock clock)
+        {
+            _clock = clock;
+            Socket = socket;
+            _isDisposed = true; // Actor is not owned by the cache
+        }
+
+        public bool TrySend(ref Msg msg, TimeSpan timeout, bool more)
+        {
+            try
+            {
+                // If we record the time, we can potentially exclude this socket from a heartbeat 
+                // if it took part in a recent transmission to save bandwidth
+                LastTransmission = _clock.Now;
+                return Socket.TrySend(ref msg, timeout, more);
+            }
+            catch (Exception e) // Change this to specific type
+            {
+                Error = true;
+                throw e; // Instead of throwing we can publish the error - but to which handler? Msg might have an answer, but we should not want to keep the reference if we want to pool the messages.
+            }
+        }
+
+        #region IDisposable Support
+
+        private bool _isDisposed = false; // To detect redundant calls
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                var disposable = Socket as IDisposable;
+                disposable?.Dispose();
+                _isDisposed = true;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/CoreDht.Node/SocketCache.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using CoreDht.Utils;
+using NetMQ;
+
+namespace CoreDht.Node
+{
+    public class SocketCache : ObjectCache<string, OutgoingSocket>, ISocketCache, IDisposable
+    {
+        private readonly IUtcClock _clock;
+
+        public SocketCache(INodeSocketFactory socketFactory, IUtcClock clock) :
+            base(key => new OutgoingSocket(socketFactory.CreateForwardingSocket(key), clock))
+        {
+            _clock = clock;
+        }
+
+        public override void Remove(string key)
+        {
+            OutgoingSocket socket;
+            if (Cache.TryGetValue(key, out socket))
+            {
+                socket.Dispose();
+                base.Remove(key);
+            }
+        }
+
+        public IEnumerable<string> PurgeFaultySockets()
+        {
+            var result = new List<string>();
+            foreach (var wrapper in Cache)
+            {
+                if (wrapper.Value.Error)
+                {
+                    result.Add(wrapper.Key);
+                }
+            }
+            result.ForEach(Remove);
+            return result;
+        }
+
+        #region IDisposable Support
+
+        private bool _isDisposed = false;
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                foreach (var socket in Cache.Values)
+                {
+                    socket.Dispose();
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This function is added so local calls can be treated the same as foreign calls
+        /// </summary>
+        /// <param name="hostAndPort">local identity host and port</param>
+        /// <param name="actor">The actor socket instance for the host</param>
+        /// 
+        public void AddActor(string hostAndPort, NetMQActor actor)
+        {
+            this.Cache.Add(hostAndPort, new OutgoingSocket(actor, _clock));
+        }
+    }
+}
