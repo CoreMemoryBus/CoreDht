f7790992617ffbb6d88ed2ab46f15a2a74f9b83c
machonky <dev@macrobert.com>
2017-07-20 08:04:54 +0800

    Ephemeral Messaging - inline message handlers

---
+++ src/CoreDht.Node/AwaitAllResponsesHandler.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreDht.Node.Messages.Internal;
+using CoreDht.Utils;
+using CoreMemoryBus;
+using CoreMemoryBus.Messages;
+
+namespace CoreDht.Node
+{
+    public class AwaitAllResponsesHandler
+        : AwaitHandler
+        , IHandle<Message>
+        , IHandle<CancelOperation>
+        , IHandle<OperationComplete>
+    {
+        private Action _initAction;
+        private Action _continuation;
+        private CorrelationId _parentCorrelation;
+        private readonly Dictionary<CorrelationId, IResponseAction> _responseActions = new Dictionary<CorrelationId, IResponseAction>();
+
+        public AwaitAllResponsesHandler(IMessageBus messageBus, Action<string> logger)
+            : base(messageBus, logger)
+        { }
+
+        public AwaitAllResponsesHandler PerformAction(Action initAction)
+        {
+            _initAction = initAction;
+            return this;
+        }
+
+        public AwaitAllResponsesHandler AndAwait<TResponse>(CorrelationId correlationId, Action<TResponse> responseCallback)
+            where TResponse : Message, ICorrelatedMessage<CorrelationId>
+        {
+            _responseActions[correlationId] = new ResponseAction<TResponse>(responseCallback);
+            Logger?.Invoke($"Awaiting {typeof(TResponse).Name} Id:{correlationId}");
+
+            return this;
+        }
+
+        public AwaitAllResponsesHandler AndAwaitAll<TResponse>(CorrelationId[] correlationIds, Action<TResponse> responseCallback)
+            where TResponse : Message, ICorrelatedMessage<CorrelationId>
+        {
+            foreach (var correlationId in correlationIds)
+            {
+                _responseActions[correlationId] = new ResponseAction<TResponse>(responseCallback);
+            }
+
+            var ids = from id in correlationIds select $"{id}";
+            Logger?.Invoke($"Awaiting All {typeof(TResponse).Name}\n\tId:{string.Join("\n\tId:", ids)}");
+
+            return this;
+        }
+
+        public AwaitAllResponsesHandler ContinueWith(Action continuation)
+        {
+            _continuation = continuation;
+            return this;
+        }
+
+        public void Run(CorrelationId parentCorrelation)
+        {
+            _parentCorrelation = parentCorrelation;
+            MessageBus.Publish(new AwaitMessage(parentCorrelation));
+
+            MessageBus.Subscribe(this);
+
+            _initAction?.Invoke();
+        }
+
+        public void Run(CorrelationId parentCorrelation, int timeoutMs)
+        {
+            _parentCorrelation = parentCorrelation;
+            MessageBus.Publish(new AwaitWithTimeoutMessage(parentCorrelation) { Timeout = timeoutMs });
+
+            MessageBus.Subscribe(this);
+
+            _initAction?.Invoke();
+        }
+
+        public void Handle(Message message)
+        {
+            if (message.GetType() != typeof(CancelOperation) ||
+                message.GetType() != typeof(OperationComplete))
+            {
+                var correlatedMessage = message as ICorrelatedMessage<CorrelationId>;
+                if (correlatedMessage != null)
+                {
+                    IResponseAction response;
+                    if (_responseActions.TryGetValue(correlatedMessage.CorrelationId, out response) && response.TryExecuteAction(message))
+                    {
+                        _responseActions.Remove(correlatedMessage.CorrelationId);
+                        if (_responseActions.Count == 0)
+                        {
+                            _continuation?.Invoke();
+                            MessageBus.Publish(new OperationComplete(_parentCorrelation));
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Handle(CancelOperation message)
+        {
+            if (message.CorrelationId.Equals(_parentCorrelation))
+            {
+                MessageBus.Unsubscribe(this);
+
+                var operationIds = from opIds in _responseActions.Keys select $"{opIds}";
+                Logger?.Invoke($"Cancel Operations\n\tId:{string.Join("\n\tId:", operationIds)}");
+            }
+        }
+
+        public void Handle(OperationComplete message)
+        {
+            if (message.CorrelationId.Equals(_parentCorrelation))
+            {
+                MessageBus.Unsubscribe(this);
+
+                Logger?.Invoke($"Operation Complete Id:{message.CorrelationId}");
+            }
+        }
+    }
+}
---
+++ src/CoreDht.Node/AwaitHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security.Policy;
+using CoreMemoryBus.Messages;
+using CoreMemoryBus.Messaging;
+
+namespace CoreDht.Node
+{
+    /// <summary>
+    /// This class provides basic common implementations. Really for framework authors, not application developers
+    /// </summary>
+    public class AwaitHandler
+    {
+        protected readonly IMessageBus MessageBus;
+        protected readonly Action<string> Logger;
+
+        protected AwaitHandler(IMessageBus messageBus, Action<string> logger)
+        {
+            MessageBus = messageBus;
+            Logger = logger;
+        }
+
+        protected interface IResponseAction
+        {
+            bool TryExecuteAction(Message replyMessage);
+        }
+
+        protected class ResponseAction<TMessage> : IResponseAction where TMessage : Message
+        {
+            private readonly Action<TMessage> _action;
+
+            public ResponseAction(Action<TMessage> action)
+            {
+                _action = action;
+            }
+
+            public bool TryExecuteAction(Message replyMessage)
+            {
+                var reply = replyMessage as TMessage;
+                if (reply != null)
+                {
+                    _action(reply);
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
