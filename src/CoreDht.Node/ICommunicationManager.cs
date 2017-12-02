22cc6f945c78fd794067e8f03960dea8ad9f264f
machonky <dev@macrobert.com>
2017-12-02 13:08:13 +0800

    Communication Manager Documentation

--- src/CoreDht.Node/ICommunicationManager.cs
+++ src/CoreDht.Node/ICommunicationManager.cs
@@ -5,12 +5,17 @@ using NetMQ;
 
 namespace CoreDht.Node
 {
+    /// <summary>
+    /// An ICommunicationManager orchestrates the sending and receipt of messages across a network.
+    /// It will manage the "negative-receipt" (aka NACK) messages and coordinate a retry in such circumstances 
+    /// where a node will be in a transient state and is incapable of processing the original message.
+    /// </summary>
     public interface ICommunicationManager
     {
         void SendInternal(Message msg);
-
-        void Receive(NetMQMessage mqMsg);
         void Send(PointToPointMessage msg);
         void Send(RoutableMessage msg);
+
+        void Receive(NetMQMessage mqMsg);
     }
 }
